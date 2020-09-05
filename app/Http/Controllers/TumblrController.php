9c25edf0a1d9bf4607cae59763ea9cc8cf692d04
Minoru Takayama <takaya030@yahoo.co.jp>
2020-09-05 18:00:49 +0900

    getting parameters from Datastore

--- app/Http/Controllers/TumblrController.php
+++ app/Http/Controllers/TumblrController.php
@@ -141,6 +141,7 @@ class TumblrController extends Controller
 
 	public function getSubscribe( Request $request )
 	{
+		/*
 		$start = (int)$request->input('start');
 		$end = (int)$request->input('end');
 		$limit = (int)$request->input('limit');
@@ -156,11 +157,33 @@ class TumblrController extends Controller
 		{
 			return;
 		}
+		 */
 
 		ini_set("max_execution_time",1800);
 
+		$datastore = new Datastore();
+		$entity = $datastore->lookup( env('DATASTORE_KIND'), env('TUMBLR_USER_ID') );
+		if( $entity instanceof Entity )
+		{
+			// start timestamp (newest)
+			$start_time  = $next_start_time = (int)$entity->get('start');
+			// end timestamp (oldest)
+			$end_time = (int)$entity->get('end');
+			// limit
+			$limit = (int)$entity->get('limit');
+		}
+		else
+		{
+			return response()->json([
+				'msg' => "fail to lookup Datastore.",
+				'kind' => env('DATASTORE_KIND'),
+				'key' => env('TUMBLR_USER_ID'),
+			]);
+		}
+
+
 		$subscriber = new PostSubscriber();
-		$raw_posts = $subscriber->getPostsBySpan( $start, $end, $limit );
+		$raw_posts = $subscriber->getPostsBySpan( $start_time, $end_time, $limit );
 		$post_objs = [];
 
 		foreach( $raw_posts as $post_item )
@@ -174,15 +197,42 @@ class TumblrController extends Controller
 
 		$blogger = new Blogger();
 		$num_objs = count($post_objs);
+		$is_error = false;
 		for( $i=0; $i<$num_objs; $i++ )
 		{
 			$result = $blogger->insertPost( $post_objs[$i] );
 			if( !$result )
 			{
+				$is_error = true;
 				break;
+			}
+			else
+			{
+				$next_start_time = $post_objs[$i]->getUnixtimestamp();
 			}
 		}
 
+		if( $is_error == false )
+		{
+			$next_start_time = $subscriber->getLastTimestamp();
+		}
+
+		// update next start
+		if( $start_time > $next_start_time )
+		{
+			$datastore->upsert( env('DATASTORE_KIND'), env('TUMBLR_USER_ID'), [
+				'start' => $next_start_time,
+				'end' => $end_time,
+				'limit' => $limit,
+			] );
+		}
+
 		//dd( $post_objs );
+		return response()->json([
+			'msg' => "success to post to blogger.",
+			'next_start' => $next_start_time,
+			'end' => $end_time,
+			'limit' => $limit,
+		]);
 	}
 }
