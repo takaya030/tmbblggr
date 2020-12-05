54f1fd0a07d6d9ac7acb6ad39e7354c162a9e403
Minoru Takayama <takaya030@yahoo.co.jp>
2020-12-05 12:41:35 +0900

    add param to retrieve posts

--- app/Console/Commands/TmbBlggr.php
+++ app/Console/Commands/TmbBlggr.php
@@ -16,7 +16,8 @@ class TmbBlggr extends Command
 	protected $signature = 'tmb:blggr
 {start_time : Start published datetime (newest) (rfc3339 yyyy-mm-ddThh:mi:ss)}
 {end_time : End published datetime (oldest) (rfc3339 yyyy-mm-ddThh:mi:ss)}
-{--limit=100 : Limit number of posts}
+{--limit=20 : Limit number of posts}
+{--retrieve=40 : Limit number of retrieve}
 {--dry-run : enable dry run}';
 
     /**
@@ -48,15 +49,17 @@ class TmbBlggr extends Command
 		$start_time = $carbon->createFromFormat( 'Y-m-d\TH:i:s', $this->argument('start_time') )->timestamp;		// from rfc3339 to unix timestamp
 		$end_time = $carbon->createFromFormat( 'Y-m-d\TH:i:s', $this->argument('end_time') )->timestamp;		// from rfc3339 to unix timestamp
 		$limit = $this->option('limit');
+		$retrieve = $this->option('retrieve');
 		//$dry_run = $this->option('dry-run');
 		$dry_run = true;
 
         $this->info("start time {$start_time}");
         $this->info("end time {$end_time}");
+        $this->info("retrieve {$retrieve}");
         $this->info("limit {$limit}");
 
 		$subscriber = new PostSubscriber();
-		$raw_posts = $subscriber->getPostsBySpan( $start_time, $end_time, $limit );
+		$raw_posts = $subscriber->getPostsBySpan( $start_time, $end_time, $retrieve );
 		$post_objs = [];
 
 		foreach( $raw_posts as $post_item )
@@ -68,18 +71,25 @@ class TmbBlggr extends Command
 			}
 		}
 
+		if( count($post_objs) > $limit )
+		{
+			$post_objs = array_slice( $post_objs, 0, $limit );
+		}
+
 		$num_objs = count($post_objs);
+		$last_timestamp = $start_time;
 		for( $i=0; $i<$num_objs; $i++ )
 		{
 			$id = $i + 1;
 			$this->info( "{$id}/{$num_objs} " . $post_objs[$i]->getDebugInfo() );
+			$last_timestamp = $post_objs[$i]->getUnixtimestamp();
 
 			if( !$dry_run )
 			{
 			}
 		}
 
-		$last_timestamp = $subscriber->getLastTimestamp();
+		//$last_timestamp = $subscriber->getLastTimestamp();
 		$carbon = new \Carbon\Carbon();
 		$time_str = $carbon->createFromTimestamp($last_timestamp, 'Asia/Tokyo')->format('Y-m-d\TH:i:sP T');
 		$this->info("last timestamp {$last_timestamp} ({$time_str})");
