f8e75c57eb18aea116e9fa69310909c62848c84c
Minoru Takayama <takaya030@yahoo.co.jp>
2021-02-23 19:33:02 +0900

    add Tumblr reblog class

---
+++ app/Http/Models/Tumblr/Reblog.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Models\Tumblr;
+
+class Reblog extends OAuthClient
+{
+	protected $blog_id = '';
+
+	public function __construct( string $blog_id = '' )
+	{
+		$this->blog_id = ($blog_id === '')? env('KONOHANA_ID') : $blog_id;
+
+		parent::__construct();
+	}
+
+	public function doReblog( $raw_post )
+	{
+		$body = [
+			'id' => $raw_post->id,
+			'reblog_key' => $raw_post->reblog_key,
+		];
+
+		$result = json_decode($this->service->request('blog/'. $this->blog_id .'.tumblr.com/post/reblog','POST', $body), true);
+
+		return( $result['meta']['status'] == 201 );
+	}
+}
